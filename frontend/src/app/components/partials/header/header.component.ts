2697dc3e219401d4cdea974980ec0914f03633fb
Medina <meditotrampova@gmail.com>
2022-11-30 14:00:54 +0200

    update login

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +11,13 @@ import { CartService } from 'src/app/services/cart.service';
 export class HeaderComponent {
   //dynamic cart
   cartQuantity = 0;
-  constructor(cartService: CartService) {
+  user!: User;
+  constructor(cartService: CartService, userService: UserService) {
     cartService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount;
+    });
+    userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
     });
   }
 }
---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+import { USER_LOGIN_URL } from '../shared/constants/urls';
+import { IuserLogin } from '../shared/interfaces/IuserLogin';
+import { User } from '../shared/models/User';
+const USER_KEY = 'User';
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private userSubject = new BehaviorSubject<User>(
+    this.getUserFromLocalStorage()
+  );
+  public userObservable: Observable<User>;
+  constructor(private http: HttpClient, private toastrService: ToastrService) {
+    this.userObservable = this.userSubject.asObservable();
+  }
+  login(userLogin: IuserLogin): Observable<User> {
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {
+          this.setUserToLocalStorage(user);
+          this.userSubject.next(user);
+          this.toastrService.success(
+            `Welcome to HealthyLiving ${user.name}!`,
+            'Login sucessful'
+          );
+        },
+        error: (errorResponse) => {
+          this.toastrService.error(errorResponse.error, 'Login failed');
+        },
+      })
+    );
+  }
+  private setUserToLocalStorage(user: User) {
+    localStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+  private getUserFromLocalStorage(): User {
+    const userJson = localStorage.getItem(USER_KEY);
+    if (userJson) return JSON.parse(userJson) as User;
+    return new User();
+  }
+}
